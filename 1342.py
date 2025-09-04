238298d44aaf9c970613f07243c83e4b89ef3490
shanto roy <shantosr3559@gmail.com>
2025-09-05 02:15:14 +0600

    771. Jewels and Stones

--- 1342.py
+++ 1342.py
@@ -1,9 +1,10 @@
-num=24
-count=0
-while num>0:
-  if num%2==0:
-    num=num//2
-  else:
-    num=num-1
-  count+=1
-print(count)
+# num=24
+# count=0
+# while num>0:
+#   if num%2==0:
+#     num=num//2
+#   else:
+#     num=num-1
+#   count+=1
+# print(count)
+                       
---
+++ 771.py
@@ -0,0 +1,8 @@
+jewels = "z"
+stones = "ZZ"
+count=0
+for i in jewels:
+  for j in stones:
+     if i==j:
+       count+=1
+print(count)      
