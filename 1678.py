ae612256a82090e4deafdcfe914388412dc6b64f
shanto roy <shantosr3559@gmail.com>
2025-09-07 23:34:18 +0600

    2798. Number of Employees Who Met the Target

--- 1678.py
+++ 1678.py
@@ -1,3 +1,6 @@
-command = "(al)G(al)()()G"
-x=command.replace("()","o").replace("(al)","al")
-print(x)
+# command = "(al)G(al)()()G"
+# x=command.replace("()","o").replace("(al)","al")
+# print(x)
+# sentence = "thequickbrownfoxjumpsoverthelazydog"
+# for i in sentence:
+#   if i=="a"
---
+++ 2798.py
@@ -0,0 +1,7 @@
+hours = [5,1,4,2,2]
+target = 6
+count=0
+for i in hours:
+  if i>=target:
+    count+=1
+print(count)    
