bad01b5a473c4fd7c1072b58d7a0e1bfa791985e
shanto roy <shantosr3559@gmail.com>
2025-09-09 22:58:37 +0600

    2114. Maximum Number of Words Found in Sentences

--- 1688.py
+++ 1688.py
@@ -1,13 +1,20 @@
-from collections import Counter
-arr = ["a","b","a"]
-k=3
-count=Counter(arr)
-D_count=0
-for i in arr:
-    if count[i]==1:
-      D_count+=1
-      if D_count==k:
-         print(i)
-         break
-else:
-    print("")
+# from collections import Counter
+# arr = ["a","b","a"]
+# k=3
+# count=Counter(arr)
+# D_count=0
+# for i in arr:
+#     if count[i]==1:
+#       D_count+=1
+#       if D_count==k:
+#          print(i)
+#          break
+# else:
+#     print("")
+sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+max_word=0
+for i in sentences:
+  words=i.split(" ")
+  count=len(words)
+  max_word=max(max_word,count)
+print(max_word)
